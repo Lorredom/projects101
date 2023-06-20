4dc6d441a25a147665efb11268d30ef287d80ad6
Lorredom <lorreain.modlen@gmail.com>
2023-06-21 00:41:35 +0200

    adds validation functions to the game improves printing of the board

--- cross-circle.py
+++ cross-circle.py
@@ -1,27 +1,49 @@
 # board = [[-1]*size]*size
+import sys
+
+def input_is_valid(row_input, col_input):
+    if row_input > 3 or col_input > 3:
+        return False
+    return True
 
 
 def play(board, player):
-    row_input, col_input = map(int, input(f"Player {player}, enter two 1-3 digits, separated by a comma: ").split(","))
-    row = row_input - 1
-    col = col_input - 1
-    # validate TODO
-    # print(player, row, col)
-    if board[row][col] == -1:
-        if player == 1:
-            board[row][col] = "X"
-        elif player == 2:
-            board[row][col] = "O"
-    else:
-        print("Field taken ")
-    
+    while True:
+        try:
+            row_input, col_input = map(int, input(f"Player {player}, enter two 1-3 digits, separated by a comma: ").split(","))
+        except ValueError:
+            continue
+
+        if not input_is_valid(row_input, col_input):
+            continue
+        
+        row = row_input - 1
+        col = col_input - 1
+
+        if board[row][col] == -1:
+            if player == 1:
+                board[row][col] = "X"
+                break
+            elif player == 2:
+                board[row][col] = "O"
+                break
+        else:
+            print("This field is taken ")
+
+
+def game_can_continue(board, size):
+    for row in range(size):
+        for col in range(size):
+            if board[row][col] == -1:
+                return True
+    return False
  
 
-def is_winner(board, size, player):
+def name_winner(board, size, player):
     for i in range(size):
         if board[i][0] == board[i][1] == board[i][2] != -1:
             return player
-        elif board[0][i] == board[1][i] == board[1][i] != -1:
+        elif board[0][i] == board[1][i] == board[2][i] != -1:
             return player
             
     arr = [0 for _ in range(3)]
@@ -36,9 +58,28 @@ def is_winner(board, size, player):
     return 0
 
 
+def print_board(board, size):
+    for row in range(size):
+        print()
+        for col in range(size):
+            if board[row][col] != -1:
+                if col != size - 1:
+                    print(board[row][col], end="|")
+                else:
+                    print(board[row][col], end="")
+            else:
+                if col != size - 1:
+                    print(" ", end="|")
+                else:
+                    print("  ", end="")
+        print()
+        if row != size - 1:
+            for _ in range(size):
+                print("--", end="")
+
+
 def main():
     BOARD_SIZE = 3
-
     # size = int(input("Enter board size: "))
 
     board = []
@@ -50,14 +91,21 @@ def main():
     winner = 0
     player = 1
     while True:
+        if not game_should_continue(board, BOARD_SIZE):
+            sys.exit("No more fields are available. Game ended\n")
+
         play(board, player)
-        winner = is_winner(board, BOARD_SIZE, player)
+        winner = name_winner(board, BOARD_SIZE, player)
         if winner != 0:
             break
         if player == 1: player = 2
         else: player = 1
-        print(board[0], board[1], board[2], sep="\n")
-    print(f"Winner is {player}")
-    print(board[0], board[1], board[2], sep="\n")
+        # print(board[0], board[1], board[2], sep="\n")
+        print_board(board, BOARD_SIZE)
+
+    if winner != 0:
+        print(f"Winner is {player}")
+    # print(board[0], board[1], board[2], sep="\n")
+    print_board(board, BOARD_SIZE)
 
 main()
