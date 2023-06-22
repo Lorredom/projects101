dbbaffdeb7feadaec178343d1dff90b6c53e64b5
Lorredom <lorreain.modlen@gmail.com>
2023-06-22 20:49:00 +0200

    Updating bookstore.py, and resulting .csv file

--- bookstore.py
+++ bookstore.py
@@ -1,3 +1,5 @@
+import csv
+
 class Book:
     def __init__(self, title, author, publication_year):
         self.title = title
@@ -6,6 +8,14 @@ class Book:
     
     def __str__(self):
         return f"{self.title} by {self.author} was published in {self.publication_year}"
+    
+    @classmethod
+    def save_new_book(cls):
+        title, author, publication_year = input("What book would you like to add?: \
+            [takes title, author's last name and publication year, separated by [, ] a comma and a space]\n").strip().split(", ")
+        new_book = cls(title, author, publication_year)
+        return new_book
+
 
 
 def is_new_book():
@@ -13,12 +23,6 @@ def is_new_book():
     if decision == "yes":
         return True
     return False
-
-def ask_for_new_book():
-    title, author, publication_year = input("What book would you like to add?: \
-        [takes title, author's last name and publication year, separate by commas]\n").strip().split(",")
-    new_book = Book(title, author, publication_year)
-    return new_book
 
 def new_entry(all_books, new_book):
     all_books.append(new_book)
@@ -29,20 +33,31 @@ def print_all_books(all_books):
 
 def main():
     all_books = []
+
+    # 1# Access .csv file
+    # 2# Create an instance for every entry (dict-->object)
+    # 3# Add objects to the all_books list
     with open("all_books.csv") as file:
-        for line in file:
-            title, author, publication_year = line.rstrip().split(",")
-            book = {"title": title, "author": author, "publicaiton_year": publication_year}
+        reader = csv.DictReader(file, fieldnames=["title", "author", "publication_year"])
+        for entry in reader:
+            book = Book(entry["title"], entry["author"], entry["publication_year"])
+            print(book)
             all_books.append(book)
+
+    # Allow adding new books, enlist them and print resulting list
     while True:
         if not is_new_book():
             break
-        new_book = ask_for_new_book()
+        new_book = Book.save_new_book()
         new_entry(all_books, new_book)
     print_all_books(all_books)
 
+    # Access file, save entries (objects) as dict
     with open("all_books.csv", "a") as file:
+        # for book in sorted(all_books, key=lambda book: book["author"]):
         for book in all_books:
-            file.write(f"{book.title}, {book.author}, {book.publication_year}\n")
+            writer = csv.DictWriter(file, fieldnames=["title", "author", "publication_year"])
+            writer.writerow({"title": book.title, "author": book.author, "publication_year": book.publication_year})
 
-main()
+if __name__ == "__main__":
+    main()
